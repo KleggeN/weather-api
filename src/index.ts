07a7a7ff93ae691b34d216915417acc7b1783422
Alper Atmaca <alper.atmaca@eliarge.com>
2025-07-01 17:13:32 +0300

    fix: added server.ts to prevent continous listening during CI test

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,4 @@ app.use('/api/auth', authRoutes)
 app.use('/api/weather', weatherRoutes)
 app.use('/api/admin', adminRoutes)
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-
 export default app
